050526398b979bb4242cc04ba5e1e8a7967774f3
ismaelbarbersanchezheredero <ismael.barber@edu.uah.es>
2025-12-15 13:58:56 +0000

    Ultimos cambios

--- prueba.py
+++ prueba.py
@@ -27,7 +27,7 @@ datos = {
             "alumno": os.environ['NAME'],
             "asignatura": os.environ['SUBJECT'],
             "nota": float(os.environ.get('NOTA', 7)),
-            "DNI": 4
+            "DNI": "012345678H"
             }
 
 @app.route('/')
